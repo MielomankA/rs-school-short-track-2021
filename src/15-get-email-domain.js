956786139741a43313df556a54f094d185bdc528
MielomankA <mielomanka@mail.ru>
2021-04-12 17:28:51 +0300

    feat: add solution 15 task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,25 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  /* throw new Error('Not implemented'); */
+
+  // Разбиваем объект email на массив строк и вводим 2 переменные со значением строка
+
+  const arr = email.split('');
+  let domain = '';
+  let sliced = '';
+  // Проходим по массиву, проверяя является ли текущий элемент @
+  // Если текущий элемент - @, извлекаем часть строки после @ методом slice
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charCodeAt(0) === 64) {
+      sliced = arr.slice(i + 1);
+    }
+  }
+
+  // Превращаем массив, содержащий информацию о домене, обратно в строку
+  domain = sliced.join('');
+  return domain;
 }
 
 module.exports = getEmailDomain;
